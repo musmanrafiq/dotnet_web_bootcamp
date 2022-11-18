4fbe2ce58bdbf25147cb74ff1ebb7157516240f7
usmanrafiq <usman.rafiq@motionsoftware.co.uk>
2022-11-18 15:54:18 +0500

    Day 1, Session 1

---
+++ DotNetWebBootcamp/Day1Exercise/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+public class Program
+{
+
+    // variables
+    int userAge = 29;
+    static string fullName = "This is my full name";
+    double salary = 1000.0;
+
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Hello, {fullName}, \n your age is {29}");
+
+        /*foreach(char singleChar in fullName)
+        {
+            Console.Write(singleChar + "\n");
+        }*/
+
+        Console.WriteLine(fullName.Substring(fullName.IndexOf('m')));
+    }
+}
+
