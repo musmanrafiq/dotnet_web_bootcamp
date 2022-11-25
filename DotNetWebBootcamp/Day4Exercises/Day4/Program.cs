6268b5f6156c73ed09941854e0ebde0b54624ec0
usmanrafiq <usman.rafiq@motionsoftware.co.uk>
2022-11-25 16:42:39 +0500

    Day 4 code

---
+++ DotNetWebBootcamp/Day4Exercises/Day4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day4
+{
+    internal class Program
+    {
+        // data structure
+        int[] ints = new int[] { 1,3};
+        static LinkedList<int> _list = new LinkedList<int>();
+        static Stack<int> _stack = new Stack<int>();
+        static Queue<int> _queue = new Queue<int>();
+
+        static void Main(string[] args)
+        {
+            _stack.Push(0);
+            _queue.Enqueue(0);
+            _list.AddFirst(0);
+
+        }
+    }
+}
