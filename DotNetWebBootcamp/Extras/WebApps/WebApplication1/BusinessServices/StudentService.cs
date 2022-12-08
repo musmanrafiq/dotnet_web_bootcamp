56d0fa23b521e318f309a710c5d24002d7772fda
usmanrafiq <usman.rafiq@motionsoftware.co.uk>
2022-12-08 13:46:00 +0500

    Testing

---
+++ DotNetWebBootcamp/Extras/WebApps/WebApplication1/BusinessServices/StudentService.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplication1.BusinessServices
+{
+    public class StudentService
+    {
+        private List<int> items;
+
+        public StudentService()
+        {
+            items = new List<int>();
+        }
+
+        public void Add(int input)
+        {
+            items.Add(input);
+        }
+
+        public List<int> Get()
+        {
+            return items;
+        }
+    }
+}
---
+++ DotNetWebBootcamp/Extras/WebApps/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.BusinessServices;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentService _studentService;
+
+        public StudentController(StudentService studentService)
+        {
+            _studentService = studentService;
+        }
+    
+        public IActionResult Index()
+        {
+            _studentService.Add(1);
+
+            return View(_studentService.Get());
+        }
+    }
+}
