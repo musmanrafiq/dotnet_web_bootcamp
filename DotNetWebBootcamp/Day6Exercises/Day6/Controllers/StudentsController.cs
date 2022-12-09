7b34b641875a73dc52e47a1fbd6f74a11d0c8654
usmanrafiq <usman.rafiq@motionsoftware.co.uk>
2022-12-09 13:12:26 +0500

    Day 6 class code

---
+++ DotNetWebBootcamp/Day6Exercises/Day6/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using Day6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day6.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add() { return View(); }
+
+        [HttpPost]
+        public IActionResult Add(StudentModel model) 
+        {
+            return View(); 
+        }
+    }
+}
---
+++ DotNetWebBootcamp/Day6Exercises/Day6/Models/StudentModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Day6.Models
+{
+    public class StudentModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
