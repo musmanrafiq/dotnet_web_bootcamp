4305861c3e980e14f862a6bb4d3fa5bdee6bc923
usmanrafiq <usman.rafiq@motionsoftware.co.uk>
2022-12-13 15:26:06 +0500

    Updates

---
+++ DotNetWebBootcamp/Extras/Benchmarking/ConsoleApp1/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    [MemoryDiagnoser(false)]
+    public class Calculator
+    {
+        [Benchmark]
+        public int Add()
+        {
+            int a =2,  b = 1;
+            return a + b;
+        }
+
+        [Benchmark]
+        public int Minus()
+        {
+            int a = 2, b = 1;
+            return a + b;
+        }
+    }
+}
---
+++ DotNetWebBootcamp/Extras/Benchmarking/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculator c = new Calculator();
+            BenchmarkRunner.Run<Calculator>();
+        }
+    }
+}
