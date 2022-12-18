48efb7d56d23a4c9aa0adc383c0bdf2f261cb9ce
usmanrafiq <usman.rafiq@motionsoftware.co.uk>
2022-12-18 12:45:49 +0500

    D6

---
+++ DotNetWebBootcamp/Day6Exercises/Day6/Controllers/D6Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day6.Controllers
+{
+    public class D6Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DotNetWebBootcamp/Extras/ConsoleApps/AllConsole/Program.cs
+++ DotNetWebBootcamp/Extras/ConsoleApps/AllConsole/Program.cs
@@ -6,25 +6,12 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        while (true)
-        {
-            Console.WriteLine("Press a to exit or any other key to continue");
 
-            ConsoleKeyInfo keyPressed = Console.ReadKey();
+        int age = 10;
+        string name = "Asad";
+        Console.WriteLine(age + name);
 
-            if (keyPressed.KeyChar == 'a')
-            {
-                Environment.Exit(0);
-            }
-
-            //Person person = new Person();
-            IPerson ali = new Student();
-            ali.Introduction();
-
-            IPerson teacher = new Teacher("Raza Ahmed", 50000);
-            teacher.Introduction();
-
-            //Person p = new Person();
-        }
+        
+       
     }
 }
