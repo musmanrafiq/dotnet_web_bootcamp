4e1a2047562b8c1182c40836724129ba56c3f535
usmanrafiq <usman.rafiq@motionsoftware.co.uk>
2023-01-20 13:04:10 +0500

    Day 19 update

--- DotNetWebBootcamp/Day16/Program.cs
+++ DotNetWebBootcamp/Day16/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.SqlClient;
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
 
 namespace Day16
 {
@@ -7,19 +9,43 @@ namespace Day16
         static void Main(string[] args)
         {
             string connectionString = "Data Source=(localdb)\\MSSQLLocalDB; Database=StudentDB;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("Select * from Student", connection);
+            connection.Open();
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            //SqlDataReader reader = cmd.ExecuteReader();
+            /*while (reader.Read())
             {
+                Console.WriteLine(reader.GetString(0));
+            }*/
 
-                SqlCommand cmd = new SqlCommand("Select Name from Student", connection);
-                connection.Open();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
 
-                SqlDataReader reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    Console.WriteLine(reader.GetString(0));
-                }
+            sqlDataAdapter.TableMappings.Add("Student", "Teacher");
+
+            DataSet dataSetForStudent = new DataSet();
+            sqlDataAdapter.Fill(dataSetForStudent, "Teacher");
+            //connection.Close();
+
+            // get no of records in data set's table no 1
+            int noOfRecords = dataSetForStudent.Tables[0].Rows.Count;
+            Console.WriteLine(noOfRecords);
+
+            var enumirator = dataSetForStudent.Tables["Teacher"].Rows;
+            foreach (DataRow row in enumirator)
+            {
+                Console.WriteLine(row["Name"]);
             }
+
+            //dataSetForStudent.Tables["Student"].Rows[0]["Name"] = "Toyota";
+            //dataSetForStudent.Tables["Student"].Rows[0]["Name"] = "MAN";
+
+            //sqlDataAdapter.Update(dataSetForStudent,"Student");
+            sqlDataAdapter.Dispose();
+            
+
+            connection.Close();
+
         }
     }
 }
---
+++ DotNetWebBootcamp/Day19Excercises/Day19EntityFramework/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Day19EntityFramework.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+       public DbSet<Employee> Employees { get; set; }
+    }
+
+    //[Table()]
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
